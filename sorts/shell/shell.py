d08af3c9cfb8638335f4f00693a19b8cc1b467cf
TheDarkRex <erykmasian@gmail.com>
2024-03-07 22:08:21 +0100

    Added shell sort

--- sorts/shell/shell.py
+++ sorts/shell/shell.py
@@ -1,2 +1,26 @@
-def shell_sort(data: list[int]) -> list[int]:
-    return []
+def shell_sort(to_sort: list) -> list:
+    n = len(to_sort)
+
+    gap = n // 2
+
+    while gap > 0:
+        j = gap
+
+        while j < n:
+            i = j - gap
+            
+            while i >= 0:
+
+                if to_sort[i+gap] > to_sort[i]:
+                    break
+
+                else:
+                    to_sort[i+gap], to_sort[i] = to_sort[i], to_sort[i+gap]
+                
+                i -= gap
+
+            j += 1
+
+        gap = gap // 2
+        
+    return to_sort 
