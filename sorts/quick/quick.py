728f52529b986626fc679bc6e3fe9cf654df853f
domelan10 <domelan10@gmail.com>
2024-03-10 20:23:17 +0100

    quick works

--- sorts/quick/quick.py
+++ sorts/quick/quick.py
@@ -1,5 +1,4 @@
 def partition(to_sort: list, left: int, right: int) -> int:
-    print(to_sort)
     pivot = to_sort[right]
     pointer = left - 1
     
@@ -9,14 +8,11 @@ def partition(to_sort: list, left: int, right: int) -> int:
             to_sort[pointer], to_sort[id] = to_sort[id], to_sort[pointer]
     
     to_sort[right], to_sort[pointer + 1] = to_sort[pointer + 1], to_sort[right]
-    print(to_sort)
     return pointer + 1
 
 def quick_sort(to_sort: list, left: int, right: int) -> None:
-    if right < left:
+    if left < right:
         pivot = partition(to_sort, left, right)
-        print(to_sort)
         
         quick_sort(to_sort, left, pivot - 1)
         quick_sort(to_sort, pivot + 1, right)
-        print(to_sort)
