c84f2d841938974ae0eab61442a0eff6ac79a5fc
domelan10 <domelan10@gmail.com>
2024-03-13 23:08:53 +0100

    quick tested

--- sorts/quick/quick.py
+++ sorts/quick/quick.py
@@ -1,18 +1,16 @@
 import random
 
 def partition(to_sort: list, left: int, right: int) -> int:
-    print(f"{left}, {right}")
     pivot = to_sort[left]
-    pointer = left + 1
+    pointer = left
     
-    for id in range(left + 1, right):
+    for id in range(left + 1, right + 1):
         if to_sort[id] <= pivot:
-            to_sort[pointer], to_sort[id] = to_sort[id], to_sort[pointer]
+            to_sort[pointer + 1], to_sort[id] = to_sort[id], to_sort[pointer + 1]
             pointer += 1
     
-    to_sort[left], to_sort[pointer - 1] = to_sort[pointer - 1], to_sort[left]
-    print(f"{pivot}: {to_sort}")
-    return pointer - 1
+    to_sort[left], to_sort[pointer] = to_sort[pointer], to_sort[left]
+    return pointer
 
 def quick_sort(to_sort: list, mode: int = 0, left: int = 0, right: int = -1) -> None:
     if right == -1:
@@ -24,6 +22,12 @@ def quick_sort(to_sort: list, mode: int = 0, left: int = 0, right: int = -1) -> 
             to_sort[left], to_sort[pivot] = to_sort[pivot], to_sort[left]
         
         pivot = partition(to_sort, left, right)
+        
+        while to_sort[pivot] == to_sort[left]:
+            pivot += 1
+            
+            if pivot >= right:
+                return
         
         quick_sort(to_sort, mode, left, pivot - 1)
         quick_sort(to_sort, mode, pivot + 1, right)
