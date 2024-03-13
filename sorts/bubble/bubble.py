ed4ee5e3c74b126910bcdbe366e4c68dbaa76496
TheDarkRex <erykmasian@gmail.com>
2024-03-13 19:51:21 +0100

    fixed flag in bubble sort - student debil moment

--- sorts/bubble/bubble.py
+++ sorts/bubble/bubble.py
@@ -1,9 +1,8 @@
 def bubble_sort(to_sort: list) -> list:
     n = len(to_sort)
-
-    swap = False
     
     for i in range(n-1):
+        swap = False
         for j in range(0,n-i-1):
             if to_sort[j] > to_sort[j+1]:
                 swap = True
@@ -11,4 +10,3 @@ def bubble_sort(to_sort: list) -> list:
 
         if not swap:
             return to_sort
-        
