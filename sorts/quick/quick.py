2840b39243f30baa415ad0c0f2c69f608e2a5fc1
domelan10 <domelan10@gmail.com>
2024-03-13 20:35:40 +0100

    quick :cry:

--- sorts/quick/quick.py
+++ sorts/quick/quick.py
@@ -1,18 +1,22 @@
 import random
 
 def partition(to_sort: list, left: int, right: int) -> int:
+    print(f"{left}, {right}")
     pivot = to_sort[left]
-    pointer = left
+    pointer = left + 1
     
-    for id in range(left, right):
-        if to_sort[id] <= pivot:
-            pointer += 1
+    for id in range(left + 1, right):
+        if to_sort[id] <= pivot and pointer < right:
             to_sort[pointer], to_sort[id] = to_sort[id], to_sort[pointer]
+            pointer += 1
     
-    to_sort[left], to_sort[pointer] = to_sort[pointer], to_sort[left]
+    to_sort[left], to_sort[pointer - 1] = to_sort[pointer - 1], to_sort[left]
     return pointer
 
 def quick_sort(to_sort: list, mode: int = 0, left: int = 0, right: int = -1) -> None:
+    if right == -1:
+        right = len(to_sort) - 1
+    
     if left < right:
         if mode == 1:
             pivot = random.randint(left, right)
