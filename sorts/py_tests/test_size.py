aa9e87733194aa9b0b8b9f64d84af8c72e7c6682
domelan10 <domelan10@gmail.com>
2024-03-19 20:50:39 +0100

    quicksort done as said

--- sorts/py_tests/test_size.py
+++ sorts/py_tests/test_size.py
@@ -1,4 +1,4 @@
 max_test_number = 100_000
 test_slope = 2500
-test_range = 10
+test_range = 20
 test_3_range = 10
--- sorts/quick/quick.py
+++ sorts/quick/quick.py
@@ -1,24 +1,23 @@
 import random
 
 def partition(to_sort: list, left: int, right: int) -> int:
-    pivot = to_sort[left]    
-    pointer_left = left + 1
+    pivot = to_sort[right]
+    pointer_left = left
     pointer_right = right
     
     while True:
-        while pointer_left <= pointer_right and to_sort[pointer_left] <= pivot:
+        while to_sort[pointer_left] < pivot:
             pointer_left += 1
         
-        while pointer_left <= pointer_right and to_sort[pointer_right] >= pivot:
+        while to_sort[pointer_right] > pivot:
             pointer_right -= 1
         
-        if pointer_right < pointer_left:
-            break
-        else:
+        if pointer_left < pointer_right:
             to_sort[pointer_left], to_sort[pointer_right] = to_sort[pointer_right], to_sort[pointer_left]
-    
-    to_sort[left], to_sort[pointer_right] = to_sort[pointer_right], to_sort[left]
-    return pointer_right
+            pointer_left += 1
+            pointer_right -= 1
+        else:
+            return pointer_right
 
 def quick_sort(to_sort: list, mode: int = 0, left: int = 0, right: int = -2) -> None:
     if right == -2:
@@ -27,11 +26,9 @@ def quick_sort(to_sort: list, mode: int = 0, left: int = 0, right: int = -2) -> 
     if left < right:
         if mode == 1:
             pivot = random.randint(left, right)
-            to_sort[left], to_sort[pivot] = to_sort[pivot], to_sort[left]
+            to_sort[right], to_sort[pivot] = to_sort[pivot], to_sort[right]
         
         pivot = partition(to_sort, left, right)
-        
-        # print(left, pivot, right)
         
         quick_sort(to_sort, mode, left, pivot - 1)
         quick_sort(to_sort, mode, pivot + 1, right)
