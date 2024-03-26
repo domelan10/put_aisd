116177b19d84458f7953529a3742d46244beb98f
domelan10 <domelan10@gmail.com>
2024-03-26 19:09:42 +0100

    fix dir

--- bin_tree/main.py
+++ bin_tree/main.py
@@ -1,37 +1,6 @@
-class Node:
-    def __init__(self, value: int, root = None):
-        self.value = value
-        self.left = None
-        self.right = None
-        self.root = root
-
-    def make_child(self, value):
-        if value < self.value:
-            self.left = Node(value, self)
-        elif value > self.value:
-            self.right = Node(value, self)
-    
-        return Node(value, self.value)
-
-    def get_value(self) -> int:
-        return self.value
-    
-    def find_max(self) -> int:
-        if self.right is not None:
-            return self.right.find_max()
-        else:
-            return self.value
-
 def main() -> None:
-    root = Node(5)
-    root.make_child(6)
-    root.make_child(7)
-    root.make_child(8)
-    root.make_child(9)
-    root.make_child(10)
-    
-    print(root.find_max())
-    
+    print("test")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
