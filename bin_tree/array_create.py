1a45c5cf677097f7fee8e7f9c0f21e8ebfdc2a11
TheDarkRex <erykmasian@gmail.com>
2024-04-21 18:50:21 +0200

    added sorting in creating array

--- bin_tree/array_create.py
+++ bin_tree/array_create.py
@@ -5,7 +5,7 @@ def create_array(n) -> list:
     # array = [n for n in range(1, n+1)]
     array = {random.randint(1, 100) for n in range(1,n+1)}
     array = list(array)
-    return array
+    return sorted(array)
 
 if __name__ == '__main__':
     print(sorted(create_array(100)))
