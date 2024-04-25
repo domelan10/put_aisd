c1c985cb216d4dda88f931eca4720b567f6e8e0b
TheDarkRex <erykmasian@gmail.com>
2024-04-25 12:47:30 +0200

    sort still not wotking, will fix it at home

--- graph/sort.py
+++ graph/sort.py
@@ -4,13 +4,19 @@ def topological_sort(array: list[list[int]], option: int) -> list[int]:
     match option:
         case 1:
             '''Adjacency Matrix'''
-            
+
             for sub_array in array:
                 if 1 not in sub_array:
                     found = array.index(sub_array)
+                    # print(found)
                     l.append(found)
                     for i in array:
                         i[found] = 0
+            
+            for sub_array in array:
+                for element in sub_array:
+                    print(element, end=' ')
+                print('\n')
 
             return l
 
